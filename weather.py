70acd1b46e1b759404b0f04091751ee7e723ab56
ag-richa-13 <agrawalricha013@gmail.com>
2021-06-22 13:33:43 +0530

    INIT COMMIT

--- weather.py
+++ weather.py
@@ -1,12 +1,13 @@
-import requests
-#import os
+# Import required moidule
+import requests, json
+#import datetime
 from datetime import datetime
 
 api_key = '87d845b0b6cf29baa1a73cc34b067a95'
-city_name = input("Enter the city name: ")
+city_name = input("Enter City Name: ")
 base_url = "https://api.openweathermap.org/data/2.5/weather?"
-complete_api_link = base_url + "q="+city_name+"&appid="+api_key
-api_link = requests.get(complete_api_link)
+complete_url = base_url + "q="+city_name+"&appid="+api_key
+api_link = requests.get(complete_url)
 api_data = api_link.json()
 
 #create variables to store and display data
@@ -16,11 +17,12 @@ hmdt = api_data['main']['humidity']
 wind_spd = api_data['wind']['speed']
 date_time = datetime.now().strftime("%d %b %Y | %I:%M:%S %p")
 
-print ("-------------------------------------------------------------")
+print ("---------------------------------------------------------------------")
 print ("Weather Stats for - {}  || {}".format(city_name.upper(), date_time))
-print ("-------------------------------------------------------------")
-
-print ("Current temperature is: {:.2f} deg C".format(temp_city))
+print ("---------------------------------------------------------------------")
+print("----------------------------------------------------------")
+print ("Current Temperature : {:.2f} deg C".format(temp_city))
 print ("Current weather desc  :",weather_desc)
 print ("Current Humidity      :",hmdt, '%')
 print ("Current wind speed    :",wind_spd ,'kmph')
+print("----------------------------------------------------------")
